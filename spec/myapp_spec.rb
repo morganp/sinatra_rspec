ba66acce9d9bfef956a282f743e0bbac6e5aad62
Jeff Dickey <jdickey@seven-sigma.com>
2012-11-26 12:44:37 +0800

    Spec file updated. - Includes SUT directly - Consistent require statements - Spacing cleaned up to current community-standard 2-space indents - Spec changed; one conceptual test per spec current best practice.

--- spec/myapp_spec.rb
+++ spec/myapp_spec.rb
@@ -1,18 +1,29 @@
 
-require File.dirname(__FILE__) + '/spec_helper'
-require File.join(File.dirname(__FILE__), '..', 'myapp.rb')
+
+require File.join File.dirname(__FILE__), 'spec_helper'
+require File.join File.dirname(__FILE__), '..', 'myapp.rb'
 
 describe "My App" do
   include Rack::Test::Methods
 
-   def app
-      MyModule::MyApp
-   end
-
-  it "should respond to /" do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should == "Hello World! a"
+  def app
+    MyModule::MyApp
   end
+
+  describe 'responds to "GET /" with' do
+    before :each do # NOT 'before :all'; cf: http://j.mp/TpcQEV
+      get '/'
+    end
+
+    it 'with an HTTP "OK" status code' do
+      last_response.should be_ok
+    end
+
+    it 'with the correct body content' do
+      last_response.body.should == "Hello World! a"
+    end
+
+  end
+
 end
 
